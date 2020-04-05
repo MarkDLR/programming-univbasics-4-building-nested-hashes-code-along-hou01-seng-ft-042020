5040fc9946862edd6dfb51484e2f7e23eb6b89db
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-05 01:38:41 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ def bonus
   }
 
   #code your solution here:
-  montague[:hero][:status] = "dead"
-  capulet[:heroine][:status] = "dead"
+  [:montague][:hero][:status] = "dead"
+  [:capulet][:heroine][:status] = "dead"
   
 
   
